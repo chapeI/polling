7cdf0b440be2417ea9fe677fe4538269b0fe7892
anoop-anthony <anoop.anthony@gmail.com>
2021-09-24 12:08:23 -0400

    if you make a servlet, map it in web.xml (servlet (name, class), servlet-mapping(name, url))

---
+++ src/main/java/business/AddServlet.java
@@ -0,0 +1,9 @@
+package business;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+
+}
