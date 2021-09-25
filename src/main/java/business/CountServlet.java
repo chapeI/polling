47647f6ca0bccd0da005e1d626c2f6f301a08d85
anoop-anthony <anoop.anthony@gmail.com>
2021-09-24 23:11:27 -0400

    'you voted blue/red'

--- src/main/java/business/CountServlet.java
+++ src/main/java/business/CountServlet.java
@@ -4,13 +4,18 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "CountServlet", value = "/CountServlet")
 public class CountServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String c = request.getParameter("color");
-        System.out.println("color" + c);
+//        String c = request.getParameter("color");
+//        System.out.println("color" + c);
+
+        PrintWriter out = response.getWriter();
+        String color = request.getParameter("color");
+        out.println("you voted " + color);
     }
 
 
