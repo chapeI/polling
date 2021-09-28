0efabfd6686990dfe99fa439c66697881cdff9bf
“Khoa <khoatrinh2108@gmail.com>
2021-09-28 19:28:28 -0400

    Initial back end

--- src/main/java/business/Poll.java
+++ src/main/java/business/Poll.java
@@ -1,34 +1,49 @@
 package business;
 
+import java.util.List;
+
 public class Poll {
-    String name;
-    String question;
-    String status = "RUNNING";
+    private String name;
+    private String question;
+    private String status;
+    private List<Choice> choices;
 
-    int red_count = 0;
-    int blue_count = 0;
-
-    Poll(String n, String q) {
-        super();
-        this.name = n;
-        this.question = q;
+    public Poll(String name, String question, String status, List<Choice> choices) {
+        this.name = name;
+        this.question = question;
+        this.status = status;
+        this.choices = choices;
     }
 
-    public void increment(int x) {
-        if(x == 1) {
-            this.red_count += 1;
-        }
-        else if (x == 2) {
-            this.blue_count += 1;
-        }
+    public String getName() {
+        return name;
     }
 
-    public void get_red_count() {
-        System.out.println("red count: " + red_count);
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String get_status() {
-        return this.status;
+    public String getQuestion() {
+        return question;
     }
 
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Choice> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(List<Choice> choices) {
+        this.choices = choices;
+    }
 }
