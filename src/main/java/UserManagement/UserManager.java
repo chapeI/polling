295b60761954e0cf3acabb918d6eb46bc931074e
Khoa Trinh <khoatrinh2108@gmail.com>
2021-12-04 02:04:35 -0500

    initial impl of UserManagement

--- src/main/java/UserManagement/UserManager.java
+++ src/main/java/UserManagement/UserManager.java
@@ -1,56 +1,79 @@
 package UserManagement;
 
 import business.UserManagerInterface;
+import com.mifmif.common.regex.Generex;
+import db.DataConn;
+
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 public class UserManager implements UserManagerInterface {
-    /**
-     * A new user may be signed-up to the system by providing name, email address,
-     * and other necessary felds, used in the previous assignments. Upon sign-up, a random
-     * verifcation token is generated and attached to the record. The verifcation token is
-     * then plugged into a verifcation link which is emailed to the user for email verifcation.
-     * The password is then entered during the verifcation
-     * @param userID
-     * @param FName
-     * @param LName
-     * @param email
-     * @param password
-     */
+
     @Override
-    public void signUp(String userID, String FName, String LName, String email, String password) {
+    public String signUp(String userID, String FName, String LName, String email, String password) {
+        String token = "";
+        boolean duplicateToken = false;
+        do {
+            // generate token
+            token = generateToken();
+            try {
+                // save info to DB -> set status to NON-VALIDATED
+                new DataConn().insertUser(userID, FName, LName, email, password, token);
+            } catch (SQLException throwables) {
+                // if token is already given to someone else
+                if (throwables instanceof SQLIntegrityConstraintViolationException) {
+                    duplicateToken = true;
+                }
+                throwables.printStackTrace();
+            }
+        } while (duplicateToken);
+
+
+        // return token to servlet
+        return token;
+    }
+
+    @Override
+    public String forgotPassword(String email) {
         // generate token
 
-        // generate verification link
+        // save token to DB
 
-        // email verification link
-
-        // email verification
-    }
-
-    /**
-     * This function is reserved to generate a change-password token. The
-     * change-password token is similarly emailed to the user. Only one change password
-     * token is allowed per user. Requesting another password change, overwrites the previous
-     * requests. There is no expiry on the password change. Once the record is marked for
-     * change, the previous password must be invalidated (i.e account is temporarily disabled).
-     * @param email
-     */
-    @Override
-    public void forgotPassword(String email) {
-        // generate token
-
-        // email token to user
-
-        //
-
+        // return token to Servlet
+        return null;
     }
 
     @Override
-    public void emailVerification(String email) {
-        //
+    public boolean emailVerification(String email, String link) {
+        // send email
+        return false;
     }
 
     @Override
-    public void changePassword(String password, String userID, String newPassword) {
+    public boolean changePassword(String password, String userID, String newPassword) {
+        // save info to DB
+        return false;
+    }
 
+    @Override
+    public boolean verifyEmail(String token) {
+        // set Validated in DB
+        return false;
+    }
+
+    @Override
+    public boolean sendEmail(String email, String link) {
+        // transform message in here
+
+        // gateway email
+
+        return false;
+    }
+
+    private String generateToken() {
+        Generex generex = new Generex("[a-zA-Z0-9]{10}");
+        String randomStr = generex.random();
+        System.out.println("Generating token: " + randomStr);
+        return randomStr;
     }
 }
