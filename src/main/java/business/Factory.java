1af3cc781066b4dd9488621a3e288118ae3be88f
Khoa Trinh <khoatrinh2108@gmail.com>
2021-12-14 13:25:51 -0500

    fixed Factory

--- src/main/java/business/Factory.java
+++ src/main/java/business/Factory.java
@@ -1,9 +1,10 @@
 package business;
 
+
 import UserManagement.UserManager;
 
 public class Factory {
-    private static UserManagerInterface UM = new UserManager();
+    private static UserManagerInterface UM = (UserManagerInterface) new UserManager();
     public static UserManagerInterface getUMObj(){
         return UM;
     }
